ffb19950463c4436ed4b6a33d4b64b5600e5584f
Pham Tri <puzuka.pham@gmail.com>
2022-08-15 13:26:07 +0700

    Init Source

---
+++ Shared/SocialAuthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAuthSwiftUIApp.swift
+//  Shared
+//
+//  Created by Pham Tri on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialAuthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
